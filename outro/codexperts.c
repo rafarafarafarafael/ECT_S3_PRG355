8a06bbef347d35ab286e0a620771197a9b953942
Rafael Santos <rafaels@gmail.com>
2026-01-17 22:56:13 -0500

    Fixed my functions for the codeXperts admission test. Turns out my approach to extract numbers from a string didn't work because of array declaration, so I had to use dynamically allocated array

--- outro/codexperts.c
+++ outro/codexperts.c
@@ -1,7 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
 
-int getLowestIndex(int numbers[]){
-	int count, minIndex = 0, arraySize;
-	arraySize = (int)(sizeof(numbers)/sizeof(int));
+int getLowestIndex(int numbers[], int arraySize);
+int prtNumbers(char str[], const int strSize);
+int isPrimeNumber(int number);
+
+int main(){
+	int intArray[] = {93, 567, 56, 87, 90, 23};
+	int minIndex, myNumber = 4;
+	char myString[] = "Here's a number for you: 23, 25";
+
+	minIndex = getLowestIndex(intArray, (int)(sizeof(intArray)/sizeof(intArray[0])));
+	printf("the lowest number is %d,\n", intArray[minIndex]);
+
+	printf("The original string is: %s\nAnd we found the following numbers in it: %d\n", myString, prtNumbers(myString, (int)(sizeof(myString)/sizeof(char))));
+
+	if(isPrimeNumber(myNumber)){
+		printf("The number %d is prime.\n", myNumber);
+	} else {
+		printf("The number %d is not prime.\n", myNumber);
+	}
+	return 0;
+}
+
+
+int getLowestIndex(int numbers[], int arraySize){
+	int count, minIndex = 0;
+	// printf("arraySize: %d\nSize of int: %d\n", arraySize, (int)(sizeof(int)));
 	for(count = 1; count < arraySize; count++){
 		if(numbers[count] < numbers[minIndex]){
 			minIndex = count;
@@ -10,18 +35,28 @@ int getLowestIndex(int numbers[]){
 	return minIndex;
 }
 
-const char* prtNumbers(char str[]){
-	const int strSize = (int)(sizeof(str)/sizeof(char));
-	char outStr[strSize];
+int prtNumbers(char str[], int strSize){
+	char* ptOutStr;
 	char curChar;
-	int count, nextChar = 0;
+	int count, nextChar = 0, outNum;
+
+	ptOutStr = (char*)calloc(strSize, sizeof(char));
+	if(NULL == ptOutStr){
+		printf("Pointer allocation failed!\n");
+		exit(1);
+	}
+
 	for(count = 0; count < strSize; count++){
 		curChar = *(str+count);
 		if(curChar >= '0' && curChar <= '9'){
-			 outStr[nextChar++] = curChar;
+			 ptOutStr[nextChar++] = curChar;
 		}
 	}
-	return outStr;
+	
+	outNum = atoi(ptOutStr);
+
+	free(ptOutStr);
+	return outNum;
 }
 
 int isPrimeNumber(int number){
